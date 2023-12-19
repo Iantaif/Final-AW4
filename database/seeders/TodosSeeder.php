fc04a6e43cd67056cb00c59480cd5fe5f4af47e1
Iantaif <95291823+Iantaif@users.noreply.github.com>
2023-12-20 00:18:48 +0700

    Add TodoFactory

--- database/seeders/TodosSeeder.php
+++ database/seeders/TodosSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Todo;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -14,20 +15,21 @@ class TodosSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('todos')->insert([
-            'title' => 'Sample Todo 1',
-            'description' => 'This is a sample todo description.',
-            'is_completed' => 0,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        Todo::factory()->count(30)->create();
+        // DB::table('todos')->insert([
+        //     'title' => 'Sample Todo 1',
+        //     'description' => 'This is a sample todo description.',
+        //     'is_completed' => 0,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
 
-        DB::table('todos')->insert([
-            'title' => 'Sample Todo 2',
-            'description' => 'Another sample todo description.',
-            'is_completed' => 1,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        // DB::table('todos')->insert([
+        //     'title' => 'Sample Todo 2',
+        //     'description' => 'Another sample todo description.',
+        //     'is_completed' => 1,
+        //     'created_at' => now(),
+        //     'updated_at' => now(),
+        // ]);
     }
 }
