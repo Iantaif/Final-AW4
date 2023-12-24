815d01e2cf0bae530a1d6c22f2398595751f7ddf
Iantaif <95291823+Iantaif@users.noreply.github.com>
2023-12-24 20:48:51 +0700

    Fix UI

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
 
-<body class="bg-gray-100 h-screen flex items-center justify-center">
-    <div class=" p-8 rounded  text-center">
-        <h1 class="text-3xl font-bold mb-6">This is Tai Todos</h1>
-        <p class="text-gray-600">Let's started</p>
+    <body class="bg-gray-100 h-screen flex items-center justify-center">
+        <div class=" p-8 rounded  text-center">
+            <h1 class="text-3xl font-bold mb-6">This is Tai Todos</h1>
+            <p class="text-gray-600">Let's started</p>
 
-        <h1 class="text-center text-4xl font-bold my-8">
-    <a class="text-black hover:text-blue-600" href="{{ url('/todos') }} " > Click here to navigate Todos</a>
-</h1>
-    </div>
-    
-</body>
-  
+            <h1 class="text-center text-4xl font-bold my-8">
+                <a class="text-black hover:text-blue-600" href="{{ url('/todos') }} "> Click here to navigate Todos</a>
+            </h1>
+        </div>
+
+    </body>
+
 </x-app-layout>
