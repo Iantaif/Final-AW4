92763174973a3dc74e7f7b356e3fe7cbb29ebec5
Iantaif <95291823+Iantaif@users.noreply.github.com>
2023-12-24 20:31:09 +0700

    Update a little bit UI

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,15 @@
 <x-app-layout>
 
 <body class="bg-gray-100 h-screen flex items-center justify-center">
-    <div class="bg-white p-8 rounded shadow-md text-center">
-        <h1 class="text-3xl font-bold mb-6">Đây là Todos Của Tài</h1>
-        <p class="text-gray-600">Bắt đầu làm việc trên những công việc của bạn ngay bây giờ!</p>
+    <div class=" p-8 rounded  text-center">
+        <h1 class="text-3xl font-bold mb-6">This is Tai Todos</h1>
+        <p class="text-gray-600">Let's started</p>
 
         <h1 class="text-center text-4xl font-bold my-8">
-    <a class="text-black hover:text-blue-600" href="{{ url('/todos') }} " >Nhấn Vào Đây để xem Todos</a>
+    <a class="text-black hover:text-blue-600" href="{{ url('/todos') }} " > Click here to navigate Todos</a>
 </h1>
     </div>
+    
 </body>
   
 </x-app-layout>
--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,130 +1,128 @@
 <x-app-layout>
-
-    <div class="py-2 bg-blue-200 flex justify-center ">
-        <p class="text-black-600 font-extrabold items-center">Index Todo</p>
-    </div>
+    <div class="container mx-auto">
 
 
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        @if(Session::has('alert-success'))
-        <div class="alert-success p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
-            {{ Session::get('alert-success') }}
+        <div class="py-2 bg-blue-200 flex justify-center mt-5 ">
+            <p class="text-black-600 font-extrabold items-center text-2xl	">This Is Your Todos List</p>
         </div>
-        @endif
-        @if(Session::has('alert-info'))
-        <div class="alert-info p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-sky-800 dark:text-blue-400" role="alert">
-            {{ Session::get('alert-info') }}
-        </div>
-        @endif
-
-        @if(Session::has('alert-error'))
-        <div class="alert-success p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-red-800 dark:text-blue-400" role="alert">
-            {{ Session::get('error') }}
-        </div>
-        @endif
-
-
-        <div class="flex justify-center items-center py-5">
-            <a href="{{ route('todos.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Create Todo
-            </a>
-        </div>
-        <div class="flex justify-center items-center py-5">
-            <a href="{{ route('categories.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                Create Category
-            </a>
-        </div>
-        <div class="flex justify-center items-center py-5">
-            <form action="{{route('todos.index')}}" method="GET" class="flex items-center">
-                <input type="text" name="search" class="border p-2 rounded-md" placeholder="Search">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
-                    Search
-                </button>
-            </form>
-        </div>
-
-
-        <form action="{{ route('todos.index') }}" method="GET" class="py-5">
-            <label>Filter by Categories:</label>
-            @foreach($categories as $category)
-            <label>
-                <input type="checkbox" name="filter_categories[]" value="{{ $category->name }}" {{ in_array($category->name, (array)request('filter_categories')) ? 'checked' : '' }}>
-                {{ $category->name }}
-            </label>
-            @endforeach
-            <input type="hidden" name="user_id" value="{{ auth()->id() }}">
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">Apply Filter</button>
-        </form>
-
-
-
-
-        <table class="w-full text-sm text-left rtl:text-right text-black dark:text-gray-400 border border-gray-500">
-            <thead class="text-xs text-black uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400 border border-gray-500">
-                <tr>
-                    <th scope="col" class="px-6 py-3 border border-gray-500">
-                        Title
-                    </th>
-                    <th scope="col" class="px-6 py-3 border border-gray-500">
-                        Descripton
-                    </th>
-                    <th scope="col" class="px-6 py-3 border border-gray-500 ">
-                        Completed
-                    </th>
-                    <th scope="col" class="px-6 py-3 border border-gray-500">
-                        Action
-                    </th>
-                    <th scope="col" class="px-6 py-3 border border-gray-500">
-                        Category
-                    </th>
-                </tr>
-            </thead>
-            @if(count($todos) > 0)
-            <!-- Display todos -->
-            @else
-            <h1 class="text-2xl">No todos match the search criteria</h1>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            @if(Session::has('alert-success'))
+            <div class="alert-success p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
+                {{ Session::get('alert-success') }}
+            </div>
             @endif
-            @if(count($todos)>0)
-            <tbody class="border border-gray-500 ">
-                @foreach ($todos as $todo)
-                <tr class="border border-gray-500 items-center">
-                    <td class="border border-gray-500">{{$todo ->title}}</td>
-                    <td class="border border-gray-500"> {{$todo -> description}}</td>
-                    <td class="border border-gray-500">
-                        @if($todo->is_completed == 1)
-                        <a class="text-green-500	" href="#">completed</a>
-                        @else
-                        <a class="text-rose-700" href="#">in completed</a>
-                        @endif
-                    </td>
-                    <td class="border border-gray-500">
-                        <a class="btn btn-success text-blue-400" href="{{ route('todos.show', $todo->id) }}">View</a>
+            @if(Session::has('alert-info'))
+            <div class="alert-info p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-sky-800 dark:text-blue-400" role="alert">
+                {{ Session::get('alert-info') }}
+            </div>
+            @endif
 
-                        <a class="btn-info text-slate-800" href="{{ route('todos.edit', $todo->id) }}">Edit</a>
-                        <form method="post" action="{{ route('todos.destroy', ['todo' => $todo->id]) }}">
-                            @csrf
-                            @method('DELETE')
-                            <input type="hidden" name="todo_id" value="{{ $todo->id }}">
-                            <input type="submit" class="btn-danger text-rose-500" value="Delete">
-                        </form>
+            @if(Session::has('alert-error'))
+            <div class="alert-success p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-red-800 dark:text-blue-400" role="alert">
+                {{ Session::get('error') }}
+            </div>
+            @endif
+            <div class="flex flex-row justify-between ml-20">
+                <div class="flex justify-center items-center py-5">
+                    <a href="{{ route('todos.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Create Todo
+                    </a>
+                </div>
+                <div class="flex justify-center items-center py-5 ">
+                    <a href="{{ route('categories.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                        Create Category
+                    </a>
+                </div>
+                <div class="flex justify-center items-center py-5">
+                    <form action="{{route('todos.index')}}" method="GET" class="flex items-center">
+                        <input type="text" name="search" class="border p-2 rounded-md" placeholder="Search">
+                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
+                            Search
+                        </button>
+                    </form>
+                </div>
+            </div>
 
-                    </td class="border border-gray-500">
 
-                    <td> {{ $todo->category ? $todo->category->name : 'Uncategorized' }}</td>
-                </tr>
-
+            <form action="{{ route('todos.index') }}" method="GET" class="py-5">
+                <label class="ml-5">Filter by Categories:</label>
+                @foreach($categories as $category)
+                <label>
+                    <input type="checkbox" class="text-red-500" name="filter_categories[]" value="{{ $category->name }}" {{ in_array($category->name, (array)request('filter_categories')) ? 'checked' : '' }}>
+                    {{ $category->name }}
+                </label>
                 @endforeach
-
-            </tbody>
-        </table>
-        @else
-        <h1 class="text-2xl	">No todos are created yet</h1>
-        <h1 class="text-2xl	">Click Search to reload the data</h1>
+                <input type="hidden" name="user_id" value="{{ auth()->id() }}">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded ml-2">Apply Your Filter</button>
+            </form>
 
 
-        @endif
 
 
+            <table class="w-full text-sm text-left rtl:text-right text-black dark:text-gray-400 border border-gray-500">
+                <thead class="text-xs text-black uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400 border border-gray-500">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 border border-gray-500">
+                            Title
+                        </th>
+                        <th scope="col" class="px-6 py-3 border border-gray-500">
+                            Descripton
+                        </th>
+                        <th scope="col" class="px-6 py-3 border border-gray-500 ">
+                            Completed
+                        </th>
+                        <th scope="col" class="px-6 py-3 border border-gray-500">
+                            Action
+                        </th>
+                        <th scope="col" class="px-6 py-3 border border-gray-500">
+                            Category
+                        </th>
+                    </tr>
+                </thead>
+                @if(count($todos) > 0)
+                <!-- Display todos -->
+                @else
+                <h1 class="text-2xl">Click Create Todo to start Your Todo</h1>
+                @endif
+                @if(count($todos)>0)
+                <tbody class="border border-gray-500 ">
+                    @foreach ($todos as $todo)
+                    <tr class="border border-gray-500 items-center">
+                        <td class="border border-gray-500">{{$todo ->title}}</td>
+                        <td class="border border-gray-500"> {{$todo -> description}}</td>
+                        <td class="border border-gray-500">
+                            @if($todo->is_completed == 1)
+                            <a class="text-green-500	" href="#">completed</a>
+                            @else
+                            <a class="text-rose-700" href="#">in completed</a>
+                            @endif
+                        </td>
+                        <td class="border border-gray-500">
+                            <a class="btn btn-success text-blue-400" href="{{ route('todos.show', $todo->id) }}">View</a>
+
+                            <a class="btn-info text-slate-800" href="{{ route('todos.edit', $todo->id) }}">Edit</a>
+                            <form method="post" action="{{ route('todos.destroy', ['todo' => $todo->id]) }}">
+                                @csrf
+                                @method('DELETE')
+                                <input type="hidden" name="todo_id" value="{{ $todo->id }}">
+                                <input type="submit" class="btn-danger text-rose-500" value="Delete">
+                            </form>
+
+                        </td class="border border-gray-500">
+
+                        <td> {{ $todo->category ? $todo->category->name : 'Uncategorized' }}</td>
+                    </tr>
+
+                    @endforeach
+
+                </tbody>
+            </table>
+            @else
+            @endif
+
+
+        </div>
     </div>
+
 
 </x-app-layout>
